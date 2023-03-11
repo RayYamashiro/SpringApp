ce7af97eac5b0249f787ed64b68044aaa6afcb10
RayYamashiro <vlad_alferov02@mail.ru>
2023-03-11 23:20:13 +0300

    @ModelAttribute + HTML form

--- src/main/java/ru/yamashiro/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/yamashiro/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.yamashiro.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.yamashiro.springcourse.dao.PersonDAO;
+import ru.yamashiro.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -32,5 +31,19 @@ public class PeopleController {
         // Получим человека по id
         model.addAttribute("person" , personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model)
+    {
+        model.addAttribute("person" , new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person)
+    {
+        personDAO.save(person);
+        return "redirect:/people"; // переходим на страницу /people
     }
 }
